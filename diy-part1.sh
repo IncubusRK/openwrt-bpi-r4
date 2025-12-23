a484694c8a1f8ca0c9ae20e8f8e2d84ad2af9e4a
Konstantin <incubusrk@gmail.com>
2025-12-23 19:53:36 +0300

    Switch mirrors
    Switch mirrors from git.openwrt.org to github.com

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,3 +16,9 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+
+# Switch mirrors
+sed -i 's|src-git packages https://git.openwrt.org/feed/packages.git|src-git packages https://github.com/openwrt/packages.git|' feeds.conf.default
+sed -i 's|src-git luci https://git.openwrt.org/project/luci.git|src-git luci https://github.com/openwrt/luci.git|' feeds.conf.default
+sed -i 's|src-git routing https://git.openwrt.org/feed/routing.git|src-git routing https://github.com/openwrt/routing.git|' feeds.conf.default
+sed -i 's|src-git telephony https://git.openwrt.org/feed/telephony.git|src-git telephony https://github.com/openwrt/telephony.git|' feeds.conf.default
